a1f40c87d085d990dbb5effdb61c9572d10d7d87
NatHigginbottom <nat@rebeluncut.co.uk>
2015-02-01 13:27:20 +0000

    v0.9

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -11,6 +11,33 @@ $(document).ready(function() {
     	$("#header #nav").slideUp("slow");
     });
 
+
+    $(".faq ul li").hide();
+
+    $(".faq ul.general li").show();
+
+    $(".faq ul h2").click(function() {
+        $(this).toggleClass("active");
+        $(this).parent().find("li").slideToggle("fast");
+    });
+
+
+    $(".day .table").hide();
+ 
+    $(".day h2").click(function() {
+        $(this).toggleClass("active");
+        $(this).parent().find(".table").slideToggle("fast");
+    });
+
+
+    $("form .dropdown > div").hide();
+
+    $("form .dropdown h2").click(function() {
+        $(this).toggleClass("active");
+        $(this).parent().children("div").slideToggle("fast");
+    });
+
+
 });
 
 $(function() {
